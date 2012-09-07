78872ba22f18ef259ff8a9db1bca6864aa47ba15
Inge Dalby <inge.dalby@gmail.com>
2012-09-07 16:00:27 +0200

    minor stuff

--- gen/com/themineralpatch/hig_assignment_1/R.java
+++ gen/com/themineralpatch/hig_assignment_1/R.java
@@ -22,11 +22,16 @@ public final class R {
         public static final int tmp=0x7f020004;
     }
     public static final class id {
-        public static final int imgHIG=0x7f080004;
-        public static final int menu_settings=0x7f080005;
-        public static final int rData=0x7f080003;
-        public static final int sData=0x7f080002;
-        public static final int wBar=0x7f080001;
+        public static final int additional=0x7f080006;
+        public static final int dataAcc=0x7f080008;
+        public static final int div1=0x7f080002;
+        public static final int div2=0x7f080007;
+        public static final int imgHIG=0x7f080005;
+        public static final int infoBtn=0x7f080001;
+        public static final int location=0x7f080009;
+        public static final int menu_settings=0x7f08000a;
+        public static final int rData=0x7f080004;
+        public static final int sData=0x7f080003;
         public static final int welcome=0x7f080000;
     }
     public static final class layout {
@@ -37,18 +42,24 @@ public final class R {
         public static final int activity_main=0x7f070000;
     }
     public static final class string {
+        public static final int additional=0x7f040006;
         public static final int app_name=0x7f040000;
+        public static final int dataAccess=0x7f04000a;
         public static final int hello_world=0x7f040001;
-        public static final int imageHIG=0x7f040007;
-        public static final int main_text01=0x7f040008;
+        public static final int imageHIG=0x7f040009;
+        public static final int main_text01=0x7f040005;
         public static final int menu_settings=0x7f040002;
-        public static final int retrieveData=0x7f040006;
-        public static final int sendData=0x7f040005;
+        public static final int myLoc=0x7f04000b;
+        public static final int retrieveData=0x7f040008;
+        public static final int sendData=0x7f040007;
         public static final int title_activity_main=0x7f040004;
         public static final int title_splash=0x7f040003;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
         public static final int CustomButton=0x7f060001;
+        public static final int divider=0x7f060004;
+        public static final int headerText=0x7f060002;
+        public static final int subHeaderText=0x7f060003;
     }
 }
