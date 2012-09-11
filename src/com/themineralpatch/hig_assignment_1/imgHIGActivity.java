39533d7ee5b0bdd9aaf9bf311c9bdd6982311166
Inge Dalby <inge.dalby@gmail.com>
2012-09-11 12:19:38 +0200

    More functionality
    Retrieving and playing an audiofile from the web implemented, as well as
    viewing a web image.

--- src/com/themineralpatch/hig_assignment_1/imgHIGActivity.java
+++ src/com/themineralpatch/hig_assignment_1/imgHIGActivity.java
@@ -1,15 +1,61 @@
 package com.themineralpatch.hig_assignment_1;
 
-import android.os.Bundle;
-import android.app.Activity;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class imgHIGActivity extends Activity{
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class imgHIGActivity extends Activity {
+
+	ImageView imageView;
+	String imgURL = "http://themineralpatch.net/img/avatar_sc2.jpg";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.imghig);
+
+		ConnectivityManager connMGR = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMGR.getActiveNetworkInfo();
+
+		if (networkInfo != null && networkInfo.isConnected()) {
+			imageView = (ImageView) findViewById(R.id.imgHIGimage);
+			new DownloadImage().execute(imgURL);
+		}
 	}
 
+	private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+		protected Bitmap doInBackground(String... url) {
+			return loadImage(url[0]);
+		}
+
+		protected void onPostExecute(Bitmap result) {
+			imageView.setImageBitmap(result);
+		}
+	}
+
+	public Bitmap loadImage(final String s) {
+		Bitmap bm = null;
+		try{
+			URL url = new URL(s);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.connect();
+			InputStream is = conn.getInputStream();
+			bm = BitmapFactory.decodeStream(is);
+		} catch (Exception e) {
+			System.out.println("Couldn't reach image: " + e);
+			e.printStackTrace();
+		}
+		return bm;
+	}
 }
--- src/com/themineralpatch/hig_assignment_1/rDataActivity.java
+++ src/com/themineralpatch/hig_assignment_1/rDataActivity.java
@@ -1,15 +1,63 @@
 package com.themineralpatch.hig_assignment_1;
 
+import java.io.IOException;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.app.Activity;
 
 public class rDataActivity extends Activity{
+	
+	String audioUrl = "http://www.themineralpatch.net/files/Terran_ScannerSweep.m4a";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.rdata);
+		
+		// Setting up the text retrieval button and creating a listener that spawns a dialog.
+		
+		// Setting up the audio playback button and creating a listener.
+		Button audioButton = (Button)findViewById(R.id.rAudioButton);
+		
+		audioButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				MediaPlayer mediaPlayer = new MediaPlayer();
+				mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+				
+				// Protection since the file might not exist
+				try {
+					mediaPlayer.setDataSource(audioUrl);
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				try {
+					mediaPlayer.prepare();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				mediaPlayer.start();
+			}
+		});
 	}
+	
+	
 	
 }
