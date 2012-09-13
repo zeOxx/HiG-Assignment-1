f4ae834956b790a03917074f6e7eb34db0df4908
Inge Dalby <inge.dalby@gmail.com>
2012-09-13 21:41:10 +0200

    senddata progress
    fixed something, fucked something else

--- src/com/themineralpatch/hig_assignment_1/sDataActivity.java
+++ src/com/themineralpatch/hig_assignment_1/sDataActivity.java
@@ -1,25 +1,25 @@
 package com.themineralpatch.hig_assignment_1;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.message.BasicNameValuePair;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+
 import android.widget.Toast;
 
 public class sDataActivity extends Activity {
@@ -46,25 +46,78 @@ public class sDataActivity extends Activity {
 	public void send(View v) {
 		// Get the message from the message text box
 		String textMessage = editMessage.getText().toString();
+		
+        // Checks if the user is connected to the internet
+     	ConnectivityManager connMgr = (ConnectivityManager)
+     			getSystemService(Context.CONNECTIVITY_SERVICE);
+     	NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+     		
+     	// If connected, show the picture
+     	if(networkInfo != null && networkInfo.isConnected()) {
 
-		// Make sure the fields are not empty
-		if (textMessage.length() > 0) {
-	          HttpClient client = new DefaultHttpClient();
-	          String getURL = "http://gtl.hig.no/mobile/logging.php?user=" + username + "&data=" + textMessage;
-	          HttpGet get = new HttpGet(getURL);
-	          try {
-				HttpResponse res = client.execute(get);
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else {
-			
-			// Display message if textbox is empty
-			Toast.makeText(getApplicationContext(), "Field cannot be empty", Toast.LENGTH_SHORT).show();
+    		// Make sure the fields are not empty
+    		if (textMessage.length() > 0) {
+    			new SendMessageToWeb().execute();
+    		} else {
+    			
+    			// Display message if textbox is empty
+    			Toast.makeText(getApplicationContext(), "Field cannot be empty", Toast.LENGTH_SHORT).show();
+    		}
+     	} 
+     	// else notify user that he is not connected, and open wireless and network settings
+     	else { 
+             AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+             alertDialog.setTitle("Internet is disabled!");
+             alertDialog.setMessage("Open settings?");
+             alertDialog.setButton(-3,"OK", new DialogInterface.OnClickListener() {
+
+            	 public void onClick(DialogInterface dialog, int which) {
+             		  startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+             		   
+            			// not sure how to refresh the activity, so finish this activity and go back to main
+            			//  when the user user clicks back from the settings page
+            			finish();
+             	  }
+             });
+             alertDialog.setIcon(R.drawable.ass1_icon);
+             alertDialog.show();
+     	}
+
+	}
+	
+	private class SendMessageToWeb extends AsyncTask <Void, Void, String> {
+
+		@Override
+		protected String doInBackground(Void...params) {
+	        try  {
+	    		String textMessage = editMessage.getText().toString();
+	        	HttpClient client = new DefaultHttpClient();
+	        	String getURL = "http://gtl.hig.no/mobile/logging.php?user=" + username + "&data=" + textMessage;
+	        	HttpGet get = new HttpGet(getURL);
+	        	HttpResponse res = client.execute(get);
+	        	
+	        	if(res != null) {
+	        		Toast.makeText(getApplicationContext(), "Something is wrong...", Toast.LENGTH_LONG).show();
+	        	}
+	        	
+	        } catch(Exception e) {
+	        	
+	        }
+
+			return null;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			Toast.makeText(getApplicationContext(), "Loading..", Toast.LENGTH_SHORT).show();
+		}
+		
+		@Override
+		protected void onPostExectute(String result) {
+			editMessage.setText("");
+			Toast.makeText(getApplicationContext(), "Message sent!", Toast.LENGTH_SHORT).show();
+			finish();
 		}
 	}
+
 }
